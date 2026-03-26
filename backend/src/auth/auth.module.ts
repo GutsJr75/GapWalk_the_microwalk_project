acc3ab9319b6e7724f2ed522f74dbe92f46e3cd7
Sadik Yamin <yaminsadik.sy@gmail.com>
2026-03-26 01:15:01 -0500

    Fix Nest auth module visibility

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FirebaseAdminService } from './firebase-admin.service';
 import { JwtStrategy } from './jwt.strategy';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
+@Global()
 @Module({
   imports: [PrismaModule],
   providers: [FirebaseAdminService, JwtStrategy, JwtAuthGuard],
